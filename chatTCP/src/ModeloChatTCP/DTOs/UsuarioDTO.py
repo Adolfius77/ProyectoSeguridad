a1e8e28c7d34ce5d67a2f0e2c4184ddf2509c8ea
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2025-12-04 12:19:43 -0700

    clase DTO y un objetoPresentacion
    los objetos presentacion se usan en las pantallas

---
+++ chatTCP/src/ModeloChatTCP/DTOs/UsuarioDTO.py
@@ -0,0 +1,2 @@
+#clase que representa a un usuario que esta conectado en el sistema
+#tiene ip, puerto, nombreUser, contraseña,
---
+++ chatTCP/src/Presentacion/ObjetosPresentacion/UsuarioOP.py
@@ -0,0 +1,2 @@
+#clase que representa a un usuario Presentacion
+#puede tener una imagen de user, nombre user, color del user, etc
